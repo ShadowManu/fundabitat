12793c3ea512c7ad98f8569572c7d7910d140328
Manuel Pacheco <manuelalejandropm@gmail.com>
2016-04-06 09:44:59 -0430

    add angular-material

--- config.js
+++ config.js
@@ -8,6 +8,19 @@ System.config({
 
   map: {
     "angular": "github:angular/bower-angular@1.5.3",
-    "css": "github:systemjs/plugin-css@0.1.20"
+    "angular-material": "github:angular/bower-material@1.0.7",
+    "css": "github:systemjs/plugin-css@0.1.20",
+    "github:angular/bower-angular-animate@1.5.3": {
+      "angular": "github:angular/bower-angular@1.5.3"
+    },
+    "github:angular/bower-angular-aria@1.5.3": {
+      "angular": "github:angular/bower-angular@1.5.3"
+    },
+    "github:angular/bower-material@1.0.7": {
+      "angular": "github:angular/bower-angular@1.5.3",
+      "angular-animate": "github:angular/bower-angular-animate@1.5.3",
+      "angular-aria": "github:angular/bower-angular-aria@1.5.3",
+      "css": "github:systemjs/plugin-css@0.1.20"
+    }
   }
 });
--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@
 import * as angular from 'angular';
 
 // Global Styles
+import 'angular-material/angular-material.css!';
 import 'app.css!';
 
+// Global Libraries
+import 'angular-material';
+
 export const NAME = "fh";
-export const DEPS = [];
+export const DEPS = [
+  'ngMaterial'
+];
 
 // Application Setup
 angular.module(NAME, DEPS);
