48daa6071c7771664266eaea49b978d3fed36d29
marylu <marylo281292@gmail.com>
2016-04-13 22:31:33 -0430

    add style to text and add attribute image to relations

--- src/statics/about/relations.ts
+++ src/statics/about/relations.ts
@@ -21,27 +21,29 @@ export function Relations():any {
 
 export class RelationsCtrl {
 
+  //TODO add images' address
+
   public nationals: any = [
-    { name: 'UCV', description: 'Universidad Central de Venezuela'},
-    { name: 'UJMV', description: 'Universidad José María Vargas' },
-    { name: 'USB', description: 'Universidad Simón Bolívar' },
-    { name: 'FUNDACIÓN TAMAYO' },
-    { name: 'INGEOLAN' },
-    { name: 'IARTES'}
+    { name: 'UCV', description: 'Universidad Central de Venezuela', image: "" },
+    { name: 'UJMV', description: 'Universidad José María Vargas', image: "" },
+    { name: 'USB', description: 'Universidad Simón Bolívar', image: "" },
+    { name: 'FUNDACIÓN TAMAYO', image: "" },
+    { name: 'INGEOLAN', image: "" },
+    { name: 'IARTES', image: ""}
   ];
 
   public internationals: any = [
-    { name: 'OEA', description: 'Organización de Estados Americanos' },
-    { name: 'UNESCO', description: 'Organización de las Naciones Unidas para la Educación, la Ciencia y la Cultura' },
-    { name: 'PNUD', description: 'Programa de las Naciones Unidas para el Desarrollo' },
-    { name: 'FAO', description: 'Organización de las Naciones Unidas para la Agricultura y la Alimentación' },
-    { name: 'OTCA', description: 'Organización del Tratado de Cooperación Amazónica' },
+    { name: 'OEA', description: 'Organización de Estados Americanos', image: "" },
+    { name: 'UNESCO', description: 'Organización de las Naciones Unidas para la Educación, la Ciencia y la Cultura', image: "" },
+    { name: 'PNUD', description: 'Programa de las Naciones Unidas para el Desarrollo', image: "" },
+    { name: 'FAO', description: 'Organización de las Naciones Unidas para la Agricultura y la Alimentación', image: "" },
+    { name: 'OTCA', description: 'Organización del Tratado de Cooperación Amazónica', image: "" },
     { name: 'CRATerre/EAG', description: 'Centre International de Construction en Terre/Ecole d´Architecture de' +
-    ' Grenoble (Francia)' }
+    ' Grenoble (Francia)', image: "" }
   ];
 
   public members: any = [
-    { name: 'UNAMAZ', description: 'Asociación de Universidades Amazónicas' },
-    { name: 'IFRAO/UNESCO', description: 'International Federation of Rock Art Organizations' }
+    { name: 'UNAMAZ', description: 'Asociación de Universidades Amazónicas', image: "" },
+    { name: 'IFRAO/UNESCO', description: 'International Federation of Rock Art Organizations', image: "" }
   ]
 }
