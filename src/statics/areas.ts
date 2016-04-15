c0d1a4297e347d6e846c57e6d65b3fe9e056660d
marylu <marylo281292@gmail.com>
2016-04-15 16:40:11 -0430

    add images in areas

--- src/statics/areas.ts
+++ src/statics/areas.ts
@@ -13,7 +13,7 @@ export function Areas(): any {
     link: function(): any {},
     controller: AreasCtrl,
 
-    controllerAs: 'ar',
+    controllerAs: 'a',
     templateUrl: 'statics/areas.html'
   };
 }
@@ -21,15 +21,17 @@ export function Areas(): any {
 export class AreasCtrl {
 
   public areas: any = [
-    "Etnoeducación",
-    "Recuperación de técnicas constructivas tradicionales",
-    "Generación y evaluación de Tecnologías Alternativas y Apropiadas",
-    "Ensayo de materiales",
-    "Saneamiento Básico ambiental",
-    "Manejo integrado de Patrimonios –natural y cultural-",
-    "Manifestaciones Rupestres",
-    "Implementación de proyectos demostrativos",
-    "Asistencia técnica a comunidades, ONG’s, OGs y privadas, para la identificación, preparación y evaluación de proyectos."
+    {name: "Etnoeducación", image: "/src/assets/images/areas/Etnoeducación.png"},
+    {name: "Recuperación de técnicas constructivas tradicionales", image: "/src/assets/images/areas/recuperación.png"},
+    {name: "Generación y evaluación de Tecnologías Alternativas y Apropiadas",
+      image: "/src/assets/images/areas/generación.png"},
+    {name: "Ensayo de materiales", image: "/src/assets/images/areas/ensayo.png"},
+    {name: "Saneamiento Básico ambiental", image: "/src/assets/images/areas/Saneamiento.png"},
+    {name: "Manejo integrado de Patrimonios –natural y cultural-", image: "/src/assets/images/areas/Manejo.png"},
+    {name: "Manifestaciones Rupestres", image: "/src/assets/images/areas/Manifestaciones.png"},
+    {name: "Implementación de proyectos demostrativos", image: "/src/assets/images/areas/implementación.png"},
+    {name: "Asistencia técnica a comunidades, ONG’s, OGs y privadas, para la identificación, preparación y evaluación" +
+    " de proyectos.", image: "/src/assets/images/areas/Asistencia.png"}
     ]
 
 }
