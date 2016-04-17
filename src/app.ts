cde660dfb859440122cad297ddeb7c6a8ef39842
marylu <marylo281292@gmail.com>
2016-04-16 21:38:55 -0430

    initial palette

--- src/app.ts
+++ src/app.ts
@@ -29,10 +29,28 @@ export const DEPS = [
 angular.module(NAME, DEPS)
     
 // Configuring Material Theme
-.config(['$mdThemingProvider', function($mdThemingProvider) {
-  
-  $mdThemingProvider.theme('default')
-    .primaryPalette('orange')
-    .accentPalette('light-green');
-}]);
 
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.definePalette('fundabitatPalette', {
+    '50': 'ffebee',
+    '100': 'ffcdd2',
+    '200': 'ef9a9a',
+    '300': 'e57373',
+    '400': 'ef5350',
+    '500': 'E49B51', //
+    '600': 'e53935',
+    '700': 'd32f2f',
+    '800': 'c62828',
+    '900': 'b71c1c',
+    'A100': 'ff8a80',
+    'A200': 'ff5252',
+    'A400': 'B53E3D', //
+    'A700': 'd50000',
+    'contrastDefaultColor': 'light',
+
+    'contrastDarkColors': ['50', '100',
+      '200', '300', '400', 'A100']
+  });
+  $mdThemingProvider.theme('default')
+    .primaryPalette('fundabitatPalette')
+});
