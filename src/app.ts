ecedbae72083121372eb5df75ae1e361c8f5ac81
marylu <marylo281292@gmail.com>
2016-04-17 12:24:41 -0430

    add more colors to fundabitatPalette

--- src/app.ts
+++ src/app.ts
@@ -32,20 +32,20 @@ angular.module(NAME, DEPS)
 
 .config(function($mdThemingProvider) {
   $mdThemingProvider.definePalette('fundabitatPalette', {
-    '50': 'ffebee',
-    '100': 'ffcdd2',
-    '200': 'ef9a9a',
-    '300': 'e57373',
-    '400': 'ef5350',
+    '50': 'ECD6C0',
+    '100': 'EACBAC',
+    '200': 'E9C19A',
+    '300': 'E5B789',
+    '400': 'E4A668',
     '500': 'E49B51', //
-    '600': 'e53935',
-    '700': 'd32f2f',
-    '800': 'c62828',
-    '900': 'b71c1c',
-    'A100': 'ff8a80',
-    'A200': 'ff5252',
+    '600': 'E69442',
+    '700': 'E4892E',
+    '800': 'E48220',
+    '900': 'E57D15',
+    'A100': 'CE7474',
+    'A200': 'BC5656',
     'A400': 'B53E3D', //
-    'A700': 'd50000',
+    'A700': 'A62929',
     'contrastDefaultColor': 'light',
 
     'contrastDarkColors': ['50', '100',
