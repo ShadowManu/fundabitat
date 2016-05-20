f1a36d08f052141c297d919550497abf844eaab2
marylu <marylo281292@gmail.com>
2016-05-19 22:13:05 -0400

    added content and changed images names

--- src/statics/publications.ts
+++ src/statics/publications.ts
@@ -24,63 +24,105 @@ export class PublicationsCtrl {
     {
       title: 'El Diseño en los Petroglifos Venezolanos',
       author: 'Ruby De Valencia y Jeannine Sujo',
-      description: 'Su contenido permitió:\n\n' +
+      description: ['Su contenido permitió:',
       '•  Presentar el primer inventario documentado fotográficamente sobre la totalidad de estaciones rupestres ' +
       'registradas a nivel nacional hasta el momento, que conforman parte del patrimonio cultural arqueológico de ' +
-      'Venezuela.\n\n' +
+      'Venezuela.',
       '•  Sustentar importantes conclusiones en el campo de la investigación antropológica referentes a Venezuela: ' +
       'en base al estudio e interrelación de las características estilísticas de estas estaciones, se formularon ' +
       'los horizontes estilísticos que definen las diferentes zonas geográficas del país. Estos horizontes ' +
       'permitieron a su vez orientar varias hipótesis referentes a algunas de las rutas migratorias más antiguas de ' +
-      'nuestro país.\n\n' +
-      'Edición trilingue español, francés e inglés de la Fundación Pampero , 410 pags, Caracas, 1987.\n\n' +
+      'nuestro país.',
+      'Edición trilingue español, francés e inglés de la Fundación Pampero , 410 pags, Caracas, 1987.', +
       'Esta publicación fue acreedora de los siguientes reconocimientos nacionales e internacionales: Representa a ' +
       'Venezuela en la Exposición EL LIBRO MAS BELLO DEL MUNDO 1988; Medalla de Bronce , Exposición Internacional ' +
       'del Arte del Libro, 1989, Leipzig, Alemania; y Reconocimiento en la IX Exposición Anual del Libro Ilustrado ' +
-      'y la Fotografía Documental , 1988, Instituto Autónomo Biblioteca Nacional, Caracas.',
-      image: ''
+      'y la Fotografía Documental , 1988, Instituto Autónomo Biblioteca Nacional, Caracas.'],
+      image: '/src/assets/images/publications/El-Diseño.png'
     },
     {
-      title: '',
-      author: '',
-      description: '',
-      image: ''
+      title: 'La Guaira Siempre',
+      author: 'Victor Hernández y Yajaira González.',
+      description: ['Este trabajo, es una recopilación de datos históricos, que a través de un excelente trabajo ' +
+      'fotográfico sobre el Patrimonio Cultural , hace un sucinto recorrido por la particular historia de laGuaira , ' +
+      'y evidencia las circunstancias que formaron a los guaireños como sociedad, desde sus más lejanas raíces, ' +
+      'hasta la actualidad, resaltando que lo más importante, no son los sucesos en sí mismos, sino saber, como ' +
+      'siempre lo hicieron sus pobladores a lo largo de su historia, sobreponerse a ellos.',
+      'Edición del CONAC -Consejo Nacional de Cultura-, 106 págs, Caracas, 2003'],
+      image: '/src/assets/images/publications/-La-Guaira-Siempre.png'
     },
     {
-      title: '',
-      author: '',
-      description: '',
-      image: ''
+      title: 'Tecnologías Apropiadas de la Amazonia en: Construcción, Saneamiento Básico Ambiental y Energías ' +
+      'Alternativas. Las Experiencias amazónicas como base de la Red de Tecnologías Apropiadas de la Amazonia –RETAM ' +
+      '(amazonia de Bolivia, Brasil, Colombia y Venezuela)',
+      author: 'Ruby De Valencia',
+      description: ['No existen soluciones universales sino Tecnologías Apropiadas para cada contexto económico, ' +
+      'organizativo, cultural y ecológico, en el cuál el elemento fundamental para su selección es el impacto ' +
+      'ambiental. Esta realidad hace necesaria la identificación del estado del arte de las Tecnologías Apropiadas, ' +
+      'y de un análisis comparativo que propicie el intercambio de experiencias que sume esfuerzos sobre la ' +
+      'materia en la región amazónica.',
+      'Este trabajo argumenta a favor del enfoque integral para atender problemas de construcción, saneamiento ' +
+      'básico ambiental –manejo de basuras, disposición de excretas, aguas servidas y energías alternas en áreas ' +
+      'marginadas de la Amazonia- por medio de Tecnologías Apropiadas y Apropiables, empleando diversas ' +
+      'ecotecnologías a través de la autogestión comunitaria del medio ambiente.',
+      'TCA - Tratado de Cooperación Amazónica -, FUNDABITAT, FAO, Ministerio de Cooperación Técnica del Reino de ' +
+      'los Países Bajos. 160 págs, Caracas, 1999'],
+      image: '/src/assets/images/publications/Tecnologías.-Apropiadas-y-Apropiables.png'
     },
     {
-      title: '',
+      title: 'CD. Base de Datos sobre Tecnologías Apropiadas de la Amazonia –Bolivia, Brasil, Colombia y Venezuela-',
       author: '',
-      description: '',
-      image: ''
+      description: ['A través de la Base de Datos, en esta primera fase, se ofrece información sobre algunas de las ' +
+      'Tecnologías Apropiadas desarrolladas en la Amazonia de Bolivia, Brasil, Colombia y Venezuela, de ' +
+      'Profesionales, Técnicos e Instituciones y Proyectos desarrollados con las mismas.',
+      'Esta información está dirigida principalmente a los Grupos Comunitarios, Técnicos y Profesionales , ' +
+      'aportando elementos a los Organismos Nacionales y Regionales que toman decisiones y elaboran los Programas ' +
+      'de desarrollo local dentro del marco de las estrategias para la reducción de la pobreza en cada país amazónico.',
+      'FUNDABITAT, FAO, TCA / SPT Venezuela–Tratado de Cooperación Amazónica / Secretaría Pro Tempore Venezuela. 2000'],
+      image: '/src/assets/images/publications/Base-de-Datos-de-tecnologías.png'
     },
     {
-      title: '',
+      title: 'CD. Base de Datos. Tecnologías Apropiadas de la Amazonia en: Construcción, Saneamiento básico ' +
+      'ambiental y energías alternas.',
       author: '',
-      description: '',
-      image: ''
+      description: ['Con el objeto de sistematizar las experiencias sobre el estado actual de las Tecnologías ' +
+      'Apropiadas, y sus actores de desarrollo en los 8 países amazónicos, FUNDABITAT estructuró una Base de Datos ' +
+      'Referencial y Relacional de Tecnologías Apropiadas en la Amazonia , sobre Construcción, Saneamiento Básico ' +
+      'Ambiental y Energías Renovables, como parte fundamental en su trabajo de configuración y puesta en marcha ' +
+      'de la RETAM –Red de tecnologías Apropiadas de la Amazonia-.',
+      'FUNDABITAT, OEA, FFPG, UNAMAZ, CAF. 2009'],
+      image: '/src/assets/images/publications/Base-de-DATOS-en-3-idiomas--.png'
     },
     {
-      title: '',
+      title: 'Juego didáctico: MANIFESTACIONES RUPESTRES VENEZOLANAS',
       author: '',
-      description: '',
-      image: ''
+      description: ['Este singular juego permitirá a los educadores, aplicar diversas estrategias, para enseñar y ' +
+      'aprender con el Patrimonio Cultural Arqueológico. Las cartas que contiene, permiten convertir el juego en ' +
+      'una experiencia educativa, en ellas encontrará información sobre las Manifestaciones Rupestres venezolanas ' +
+      'y los estados donde se ubican.',
+      'Estudiantes, educadores y público en general de todas las edades, podrán entrar al mundo de las ' +
+      'Manifestaciones Rupestres, a través de este juego didáctico de divulgación científica, que propone fomentar ' +
+      'el conocimiento, como vehículo para la protección de los sitios rupestres en Venezuela.',
+      'FUNDABITAT, 2012'],
+      image: '/src/assets/images/publications/RUEDA-VENEZUELA-copia-1.png'
     },
     {
-      title: '',
+      title: 'Juego didáctico: VENEZUELA RUPESTRE y MUNDO RUPESTRE',
       author: '',
-      description: '',
-      image: ''
+      description: ['A través de ruedas giratorias, este juego didáctico propone un recorrido temático por la ' +
+      'Venezuela Rupestre y el Mundo Rupestre, muestra su unidad, su diversidad, y la forma de comprender el mundo,' +
+      ' a través de las Manifestaciones Rupestres.',
+      'FUNDABITAT, 2012'],
+      image: '/src/assets/images/publications/RUEDA-MUNDO.png'
     },
     {
-      title: '',
+      title: 'CD - Memoria Institucional TRATADO DE COOPERACIÓN AMAZÓNICA Publicaciones 1990-1999',
       author: '',
-      description: '',
-      image: ''
+      description: ['Este CD-ROM contiene las publicaciones de la Secretaría Pro Tempore del Tratado de Cooperación ' +
+      'Amazónica (algunas de FUNDABITAT) entre 1990 y 1999, años durante los cuales la Secretaría ha sido ejercida ' +
+      'por Ecuador, Perú y Venezuela. Dichas publicaciones están dirigidas a todos los estudiosos del hecho amazónico.',
+      'TCA, 1999'],
+      image: '/src/assets/images/publications/Tratado-de.png'
     },
   ]
 
