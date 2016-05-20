718bb7a1793a4ac2bfeba79e35dd2b4c418cc6c3
marylu <marylo281292@gmail.com>
2016-05-19 22:26:28 -0400

    added images

--- src/statics/about/team.ts
+++ src/statics/about/team.ts
@@ -25,33 +25,33 @@ export class TeamCtrl {
       name: 'Ruby De Valencia',
       description: 'Arquitecto Paisajista. Especialista en Tecnologías Apropiadas y Apropiables. Co-fundadora y ' +
       'Directora del ANAR',
-      photo: ''
+      photo: '/src/assets/images/team/Ruby-De-Valencia.png'
 
     },
     {
       name: 'José Gregorio Camacho-Uzcategui',
       description: 'Abogado Mercantil, Co-fundador y Asesor legal de FUNDABITAT',
-      photo: ''
+      photo: '/src/assets/images/team/José-Camacho.png'
     },
     {
       name: 'Mira Kugelmann',
       description: 'Licenciada en Educación integral. Coordinadora de Idiomas y Directora de CEGAVEN',
-      photo: ''
+      photo: '/src/assets/images/team/Mira-Kugelmann.png'
     },
     {
       name: 'Luis Müller Peruga',
       description: 'Diseñador Gráfico y Comunicador Visual',
-      photo: ''
+      photo: '/src/assets/images/team/Luis-Müller.png'
     },
     {
       name: 'Paola Valencia',
       description: 'Artista Plástico y Comunicadora Visual',
-      photo: ''
+      photo: '/src/assets/images/team/Paola-Valencia.png'
     },
     {
       name: 'Victor Hernández Turupial',
       description: 'Fotógrafo con interés en Ecología y Patrimonio Cultural',
-      photo: ''
+      photo: '/src/assets/images/team/Victor-Hernández.png'
     },
   ]
 }
